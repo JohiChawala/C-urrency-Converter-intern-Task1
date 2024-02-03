1b1e2dc69be7e2559da2897a6b6e91d9a8b35455
JohiChawala <136928760+JohiChawala@users.noreply.github.com>
2024-02-04 02:31:24 +0500

    change position of labels in html

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,24 +1,19 @@
-// include api for currency change 
-
+//api 
 const api = "https://api.exchangerate-api.com/v4/latest/USD"; 
-
-  
-// for selecting different controls 
-
+ 
+// making variables 
 var search = document.querySelector(".searchBox"); 
-
 var convert = document.querySelector(".convert"); 
-
 var fromCurrecy = document.querySelector(".from"); 
-
 var toCurrecy = document.querySelector(".to"); 
-
 var finalValue = document.querySelector(".finalValue"); 
-
 var finalAmount = document.getElementById("finalAmount"); 
-
 var resultFrom; 
-
 var resultTo; 
-
 var searchValue; 
+
+// Event when currency is changed 
+fromCurrecy.addEventListener('change', (event) => { 
+    resultFrom = `${event.target.value}`; 
+}); 
+
