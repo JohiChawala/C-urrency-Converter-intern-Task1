082ef1f527bc2a19f3fe38ffc3bcea6262d3832e
JohiChawala <136928760+JohiChawala@users.noreply.github.com>
2024-02-04 02:17:20 +0500

    create variables in js

---
+++ assests/js/script.js
@@ -0,0 +1,24 @@
+// include api for currency change 
+
+const api = "https://api.exchangerate-api.com/v4/latest/USD"; 
+
+  
+// for selecting different controls 
+
+var search = document.querySelector(".searchBox"); 
+
+var convert = document.querySelector(".convert"); 
+
+var fromCurrecy = document.querySelector(".from"); 
+
+var toCurrecy = document.querySelector(".to"); 
+
+var finalValue = document.querySelector(".finalValue"); 
+
+var finalAmount = document.getElementById("finalAmount"); 
+
+var resultFrom; 
+
+var resultTo; 
+
+var searchValue; 
