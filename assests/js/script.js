f20637918f4638f4dde0275be6fbad773af044a8
JohiChawala <136928760+JohiChawala@users.noreply.github.com>
2024-02-05 18:13:28 +0500

    event when currency is changed

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,3 +1,19 @@
 // include api for currency change 
 const api = "https://api.exchangerate-api.com/v4/latest/USD"; 
 
+// for selecting different controls 
+var search = document.querySelector(".searchBox"); 
+var convert = document.querySelector(".convert"); 
+var fromCurrecy = document.querySelector(".from"); 
+var toCurrecy = document.querySelector(".to"); 
+var finalValue = document.querySelector(".finalValue"); 
+var finalAmount = document.getElementById("finalAmount"); 
+var resultFrom; 
+var resultTo; 
+var searchValue; 
+
+// Event when currency is changed 
+fromCurrecy.addEventListener('change', (event) => { 
+    resultFrom = `${event.target.value}`; 
+}); 
+  
